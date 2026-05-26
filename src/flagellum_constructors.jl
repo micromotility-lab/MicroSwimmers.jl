f5e2f46595651ae3a38cd848898b587b8cfefed7
James Cass <jamesfcass@gmail.com>
2026-05-26 11:56:00 +0100

    removed superfluous constructor for Flagellum, removed Tube constructors

--- src/flagellum_constructors.jl
+++ src/flagellum_constructors.jl
@@ -7,11 +7,9 @@ end
 
 function update_boundary!(f::BareFlagellum, t::T) where {T <: Number}
     f.model(f.points, t)
-    # f.model(f.points.force_pts, f.points.velocity, t)
-    # f.model(f.points.quad_pts, t)
 end
 
-# Provide N and Q for a nearest discretisation
+# Provide N and Q for the nearest discretisation
 function Flagellum(
     model=PlanarFlagellum(1., 0., 0.3, 0.15, 2π, 2π, 2π, 0.0),
     N::Int=23, 
@@ -27,36 +25,6 @@ function Flagellum(
     f
 end
 
-
-# Only provide N for a Nystrom discretisation
-function Flagellum(
-    model::FlagellumModel,
-    N::Int;
-    location=SVector(0., 0., 0.),
-    orientation=I3
-)
-    points = NystromDiscretisation(N; location=SVector{3}(location), orientation=orientation)
-    f = BareFlagellum(model, points)
-    update_boundary!(f, 0.)
-    f
-end
-
-function Flagellum(::Type{T},
-    model,
-    N, 
-    Q; 
-    location=SVector{3,T}(0, 0, 0), orientation=SMatrix{3,3,T}(I)
-) where {T <: Number}
-    points = NearestDiscretisation(
-        T, N, Q; 
-        location=SVector{3,T}(location), orientation=SMatrix{3,3,T}(orientation)
-    )
-    
-    f = BareFlagellum(model, points)
-    update_boundary!(f, T(0.0))
-    nearest_neighbour!(f.points)
-    f
-end
 
 struct VanedFlagellum{M <: FlagellumModel, D <: Discretisation} <: Flagellum
     model::M
@@ -115,113 +83,3 @@ get_vane_pts(vf::VanedFlagellum) = hcat(
     vf.points.force_pts[:, vf.N_start:vf.N_start + vf.N_v - 1],  
     vf.points.force_pts[:, vf.N_f+1:end]
 )
-
-struct TubeFlagellum{M <: FlagellumModel, D <: Discretisation, T <: Number} <: Flagellum
-    model::M
-    points::D
-
-    N_cs::Int
-    Q_cs::Int
-    radius::T
-end
-
-function TubeFlagellum(
-    model=PlanarFlagellum(1., 0., 0.3, 0.15, 2π, 2π, 2π, 0.0),
-    N=23, 
-    N_cs=5,
-    Q=127,
-    Q_cs=12; 
-    location=SVector(0., 0., 0.),
-    orientation=I3,
-    radius=0.1
-)
-    @info "" N*N_cs Q*Q_cs
-    points = NearestDiscretisation(
-        N*N_cs, Q*Q_cs;
-        location=SVector{3}(location), orientation=orientation
-    )
-
-    tf = TubeFlagellum(
-        model, points, 
-        N_cs, Q_cs, radius
-    )
-
-    update_boundary!(tf, 0.)
-    nearest_neighbour!(tf.points)
-    tf
-end
-
-function update_boundary!(tf::TubeFlagellum, t::T) where {T <: Number}
-    @unpack N_cs, Q_cs, radius, points = tf
-    tf.model(points.force_pts, points.velocity, N_cs, t, radius=radius)
-    tf.model(points.quad_pts, Q_cs, t, radius=radius)
-end
-
-# function TubeFlagellum( 
-#     model=PlanarFlagellum(1., 0., 0.3, 0.15, 2π, -2π, 2π, 0.0),
-#     N=23, 
-#     N_cs=5,
-#     Q=127,
-#     Q_cs=12; 
-#     location=SVector(0., 0., 0.),
-#     orientation=I3,
-#     radius=0.1
-# )
-#     points = TubeFlagellumNearestDiscretisation(
-#         N, N_cs, Q, Q_cs; 
-#         location=SVector{3}(location), 
-#         orientation=orientation,
-#         radius=radius
-#     )
-#     f = BareFlagellum(model, points)
-
-#     update_boundary!(f, 0.)
-#     nearest_neighbour!(f.points)
-#     f
-# end
-
-# function TubeFlagellum(::Type{T}, 
-#     model,  
-#     N, 
-#     N_cs,
-#     Q,
-#     Q_cs; 
-#     location=SVector{3,T}(0, 0, 0), orientation=SMatrix{3,3,T}(I),
-#     radius=T(0.1)
-# ) where {T <: Number}
-#     points = TubeFlagellumNearestDiscretisation(
-#         N, N_cs, Q, Q_cs; 
-#         location=SVector{3, T}(location), 
-#         orientation=SMatrix{3,3,T}(orientation),
-#         radius=radius
-#     )
-#     f = BareFlagellum(model, points)
-
-#     update_boundary!(f, zero(T))
-#     nearest_neighbour!(f.points)
-#     f
-# end
-
-function LineTubeFlagellum( 
-    model=PlanarFlagellum(1., 0., 0.3, 0.15, 2π, -2π, 2π, 0.0),
-    N=23, 
-    Q=127,
-    Q_cs=5; 
-    location=SVector(0., 0., 0.),
-    orientation=I3,
-    radius=0.1
-)
-    
-    points = LineTubeFlagellumNearestDiscretisation(N, Q, Q_cs; 
-        location=SVector{3}(location), orientation=orientation,
-        radius=radius
-    )
-    f = BareFlagellum(model, points)
-
-    update_boundary!(f, 0.)
-    nearest_neighbour!(f.points)
-    f
-end
-
- 
-
