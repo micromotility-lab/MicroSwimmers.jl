012db36e937145963a85568a85508486f83043d0
James Cass <jamesfcass@gmail.com>
2026-05-26 11:17:26 +0100

    removed NystromDiscretisation and repeated code in numerics.jl

--- src/discretisations.jl
+++ src/discretisations.jl
@@ -1,30 +1,5 @@
 abstract type Discretisation end
 
-mutable struct NystromDiscretisation{T <: Number} <: Discretisation
-    N::Int
-    location::SVector{3,T}
-    orientation::SMatrix{3,3,T}
-    force_pts::Matrix{T}
-    velocities::Matrix{T}
-end
-
-NystromDiscretisation(N::Int; location::SVector{3, T}=SVector(0.,0.,0.), orientation=I3) where {T <: Number} = NystromDiscretisation(
-    N,
-    location,
-    orientation,
-    zeros(T,3,N),
-    zeros(T,3,N)
- )
-
- NystromDiscretisation(force_pts::Matrix{T}, velocities::Matrix{T}; location::SVector{3, T}=SVector(0.,0.,0.), orientation=I3) where {T <: Number} = NystromDiscretisation(
-    size(force_pts,2),
-    location,
-    orientation,
-    force_pts,
-    velocities
- )
-
-
 mutable struct NearestDiscretisation{T <: Number} <: Discretisation
     N::Int                          # number of force points
     Q::Int                          # number of quadrature points
@@ -36,41 +11,24 @@ mutable struct NearestDiscretisation{T <: Number} <: Discretisation
     nearest::Vector{Int}            # length Q
 end
 
-traction_nodes(points::NystromDiscretisation)  = points.force_pts       # 3×N
-
+# convenience functions
+traction_nodes(points::NearestDiscretisation)  = points.force_pts       # 3×N
 n_traction_nodes(points) = size(traction_nodes(points), 2)
 n_unknowns(points) = 3 * n_traction_nodes(points)
 
 
-NearestDiscretisation(N::Int, Q::Int; location::SVector{3, T}=SVector(0.,0.,0.), orientation=I3) where {T <: Number} = NearestDiscretisation(
+# Constructors
+NearestDiscretisation(N::Int, Q::Int; location=SVector(0.,0.,0.), orientation=I3) where {T <: Number} = NearestDiscretisation(
     N,
     Q,
-    location,
-    orientation,
+    SVector{3}(location),
+    SMatrix{3,3}(orientation),
     zeros(T, 3, N),
     zeros(T, 3, N),
     zeros(T, 3, Q),
     zeros(Int, Q)
 )
     
-function NearestDiscretisation(::Type{T}, 
-    N::Int, Q::Int; 
-    location=SVector{3,T}(0, 0, 0), orientation=SMatrix{3,3,T}(I)
-    ) where {T <: Number}
-    NearestDiscretisation(
-        N,
-        Q,
-        location,
-        orientation,
-        zeros(T, 3, N),
-        zeros(T, 3, N),
-        zeros(T, 3, Q),
-        zeros(Int, Q)
-    )
-end
-    
-traction_nodes(points::NearestDiscretisation) = points.force_pts  # 3×Nf
-
 NearestDiscretisation(force_pts::AbstractMatrix{T}, quad_pts::AbstractMatrix{T}; location=SVector(0.,0.,0.), orientation=I3) where {T <: Number} = NearestDiscretisation(
     size(force_pts, 2),
     size(quad_pts, 2),
@@ -82,7 +40,6 @@ NearestDiscretisation(force_pts::AbstractMatrix{T}, quad_pts::AbstractMatrix{T};
     nearest_neighbour(force_pts, quad_pts)
 )
 
-
 NearestDiscretisation(force_pts, quad_pts, nearest; location=SVector(0.,0.,0.), orientation=I3) = NearestDiscretisation(
     size(force_pts, 2),
     size(quad_pts, 2),
@@ -93,27 +50,8 @@ NearestDiscretisation(force_pts, quad_pts, nearest; location=SVector(0.,0.,0.), 
     quad_pts,
     nearest
 )
-        
-function spacing(points::NearestDiscretisation)
-    @unpack force_pts, quad_pts, N, Q, nearest = points
-    dnn = [minimum(norm.(eachcol(force_pts .- force_pts[:, i]))[setdiff(1:N, i)]) for i in 1:N]
-    hf = (minimum(dnn), median(dnn), maximum(dnn))
-    @info "" findmin(dnn)
-    @info "hf (min, median, max)" hf
-    # hf = median(dnn)
 
-    dnn = Float64[]
-    for i in 1:N
-        patch_quad_pts = quad_pts[:, nearest .== i]
-        Qp = size(patch_quad_pts, 2)
-        append!(dnn, [minimum(norm.(eachcol(patch_quad_pts .- patch_quad_pts[:, j]))[setdiff(1:Qp, j)]) for j in 1:Qp])
-    end
-    @info "" findmax(dnn)
-    hq = (minimum(dnn), median(dnn), maximum(dnn))
-    @info "hq (min, median, max)" hq
-    hf, hq
-end
-
+# spacing between force points, returns (min, med, max)
 function hf(points::NearestDiscretisation)
     @unpack force_pts, N, = points
     dnn = [minimum(norm.(eachcol(force_pts .- force_pts[:, i]))[setdiff(1:N, i)]) for i in 1:N]
@@ -121,6 +59,7 @@ function hf(points::NearestDiscretisation)
     (minimum(dnn), median(dnn), maximum(dnn))
 end
 
+# spacing between quadrature points, returns (min, med, max)
 function hq(points::NearestDiscretisation)
     @unpack force_pts, quad_pts, N, Q, nearest = points
     dnn = Float64[]
@@ -134,105 +73,7 @@ function hq(points::NearestDiscretisation)
 end
 
 
-
-
-
-# # Forward unknown properties to `points`
-# @inline function Base.getproperty(f::VanedFlagellumNearestDiscretisation, name::Symbol)
-#     name in (:points, :N_f, :Q_f, :N_v, :N_start, :N_height, :Q_v, :Q_start, :Q_height) ? getfield(f, name) : getproperty(f.points, name)  
-# end
-
-# @inline function Base.setproperty!(f::VanedFlagellumNearestDiscretisation, name::Symbol, value)
-#     name in (:points, :N_f, :Q_f, :N_v, :N_start, :N_height, :Q_v, :Q_start, :Q_height) ? setfield!(f, name, value) : setproperty!(f.points, name, value)
-# end
-
-# function VanedFlagellumNearestDiscretisation(
-#     N_f::Int,
-#     Q_f::Int, 
-#     N_v::Int, 
-#     N_start::Int, 
-#     N_height::Int; 
-#     location=SVector(0.,0.,0.), 
-#     orientation=I3
-# ) 
-#     Q_v = floor(Int, (N_v / N_f) * Q_f) 
-#     Q_start = ceil(Int, ((N_start-1) / (N_f-1)) * (Q_f-1))
-#     Q_height = floor(Int, (N_height / N_f) * Q_f)
-
-#     points = NearestDiscretisation(
-#         N_f + N_height*N_v, Q_f + Q_height*Q_v;
-#         location=location, orientation=orientation
-#     )
-
-#     VanedFlagellumNearestDiscretisation(points, N_f, Q_f, N_v, N_start, N_height, Q_v, Q_start, Q_height)
-# end
-
-
-struct TubeFlagellumNearestDiscretisation{T <: Number} <: Discretisation
-    points::NearestDiscretisation
-
-    N_cs::Int  # Number of cross section force points on the tube
-    Q_cs::Int  # Number of cross section quadrature points on the tube
-    radius::T  # cross-sectional radius of the tube
-end
-
-# Forward unknown properties to `points`
-@inline function Base.getproperty(f::TubeFlagellumNearestDiscretisation, name::Symbol)
-    name in (:points, :N_cs, :Q_cs, :radius) ? getfield(f, name) : getproperty(f.points, name)  
-end
-
-@inline function Base.setproperty!(f::TubeFlagellumNearestDiscretisation, name::Symbol, value)
-    name in (:points, :N_cs, :Q_cs, :radius) ? setfield!(f, name, value) : setproperty!(f.points, name, value)
-end
-
-
-function TubeFlagellumNearestDiscretisation(
-    N::Int,
-    N_cs::Int, 
-    Q::Int, 
-    Q_cs::Int; 
-    location=SVector(0.,0.,0.), 
-    orientation=I3,
-    radius=0.01
-)
-
-    points = NearestDiscretisation(N*N_cs, Q*Q_cs, location=location, orientation=orientation)
-    TubeFlagellumNearestDiscretisation(points, N_cs, Q_cs, radius)
-end
-
-
-struct LineTubeFlagellumNearestDiscretisation{T <: Number} <: Discretisation
-    points::NearestDiscretisation
-
-    Q_cs::Int  # Number of cross section quadrature points on the tube
-    radius::T  # cross-sectional radius of the tube
-end
-
-# Forward unknown properties to `points`
-@inline function Base.getproperty(f::LineTubeFlagellumNearestDiscretisation, name::Symbol)
-    name in (:points, :Q_cs, :radius) ? getfield(f, name) : getproperty(f.points, name)  
-end
-
-@inline function Base.setproperty!(f::LineTubeFlagellumNearestDiscretisation, name::Symbol, value)
-    name in (:points, :Q_cs, :radius) ? setfield!(f, name, value) : setproperty!(f.points, name, value)
-end
-
-
-function LineTubeFlagellumNearestDiscretisation(
-    N::Int, 
-    Q::Int, 
-    Q_cs::Int; 
-    location=SVector(0.,0.,0.), 
-    orientation=I3,
-    radius=0.01
-)
-    points = NearestDiscretisation(N, Q*Q_cs, location=location, orientation=orientation)
-    LineTubeFlagellumNearestDiscretisation(points, Q_cs, radius)
-end
-
-
-
-
+# Calculate the nearest neighbour vector
 function nearest_neighbour(force_pts::AbstractMatrix{T}, quad_pts::AbstractMatrix{T}) where {T <: Number}
     nearest = Int[]
     for x in eachcol(quad_pts)
@@ -242,6 +83,7 @@ function nearest_neighbour(force_pts::AbstractMatrix{T}, quad_pts::AbstractMatri
     nearest
 end 
 
+# calculate the neighbour vector for a vector of parts
 function nearest_neighbour(force_pts::AbstractVector{<:AbstractMatrix{T}}, quad_pts::AbstractVector{<:AbstractMatrix{T}}) where {T <: Number}
     nearest = Int[]
     idx = 0
--- src/numerics.jl
+++ src/numerics.jl
@@ -110,104 +110,6 @@ function regularised_blakelet!(B, T, x, X; eps=1e-6)
 end
 
 
-# function regularised_blakelet!(B, T, x, X; eps=1e-6)
-#     @assert length(x) == 3 "x must be a 3D vector"
-#     @assert length(X) == 3 "X must be a 3D vector"
-
-#     @info "" eps
-
-#     # Primary stokeslet
-#     R = x .- X
-#     regularised_stokeslet!(B, R; eps=eps)
-    
-#     Delta = Diagonal([1., 1., -1.])  
-#     # Image stokeslet
-#     Rimg     = x .- [X[1]; X[2]; -X[3]]
-#     regularised_stokeslet!(T, Rimg; eps=eps)
-#     B .-= T
-    
-#     # Some local shortcuts
-#     h   = X[3]          # height of source point above z=0
-#     R2  = sum(Rimg.^2)
-#     dist = sqrt(R2 + eps^2)
-#     iReps   = 1/dist
-#     iReps3  = iReps^3
-#     iReps5  = iReps^5
-#     X1, X2, X3 = Rimg
-
-#     # 1) Blob term:    -2*h^2 * Delta * (3 eps^2 iReps^5)
-#     phi = 3eps^2 * iReps5
-#     B .+= -2h^2 .* (phi *Delta)
-
-#     # 2) Potential source dipole
-#     #    2*h^2 [Delta*iReps3 - 3 iReps5 * (X1X1, X1X2, -X1X3; ...)]
-#     M = Rimg .* Rimg'
-#     M[:,3] .*= -1
-
-#     B .+= 2h^2 .*(iReps3 * Delta .- 3iReps5 .* M)
-
-#     # 3) Stokes dipole
-#     #    2*h * [ (complicated expression) ]
-#     # For brevity, we define a small local helper:
-
-#     val = R2 + 4eps^2
-#     T .= [
-#         0 0 0;
-#         0 0 0;
-#         X1*val*iReps5   X2*val*iReps5   -X3*val*iReps5
-#     ]
-#     T .-= (X3 * iReps3) * Delta
-#     T[3,:] .+= [X1*iReps3;  X2*iReps3;  X3*iReps3]
-#     T .+= 3.0*iReps5*X3*M
-#     T .*= 2h
-#     B .+= T
-
-#     # 4) Rotlet difference
-#     #    -(6 * h * eps^2 * iReps5)*( [0;0; X1 X2 X3] - diag(X3,X3,X3) )
-#     T .= [
-#         0 0 0;
-#         0 0 0;
-#         X1 X2 X3
-#     ]
-#     T[diagind(T)] .-= X3
-#     T *= -6*h*eps^2*iReps5
-#     B .+= T
-# end
-
-# function regularised_blakelet!(B::StaticMatrix, T::StaticMatrix, x::StaticVector, x₀::StaticVector; eps=1e-6)
-#     h = x₀[3]
-#     r = x - x₀
-#     r_norm² = norm(r)^2 + eps^2
-#     r_norm = sqrt(r_norm²)
-    
-#     # Image configuration
-#     x₀_image = SVector(x₀[1], x₀[2], -x₀[3])
-#     r_image = x - x₀_image
-#     r_image_norm² = norm(r_image)^2 + eps^2
-#     r_image_norm = sqrt(r_image_norm²)
-    
-#     I₃ = SMatrix{3,3}(1.0I)
-    
-#     # Regularized Stokeslet
-#     G_reg = ((r_norm² + 2eps^2) * I₃ + (r * r')) / (r_norm^3)
-    
-#     # Regularized image Stokeslet
-#     G_image_reg = ((r_image_norm² + 2eps^2) * I₃ + (r_image * r_image')) / (r_image_norm^3)
-    
-#     # Regularized doublet terms
-#     G_doublet_reg = -2h * (
-#         (r_image_norm² + 2eps^2) * I₃ - 3 * (r_image * r_image')
-#     ) / (r_image_norm^5)
-    
-#     e₃ = SVector(0.0, 0.0, 1.0)
-#     G_source_doublet_reg = 2h * (
-#         (e₃ * r_image') + (r_image * e₃') - 
-#         2 * (e₃ ⋅ r_image) * (r_image * r_image') / r_image_norm²
-#     ) / (r_image_norm^3)
-    
-#     B .= G_reg - G_image_reg + G_doublet_reg + G_source_doublet_reg 
-# end
-
 function resistance_matrix!(
     A::AbstractMatrix{T},
     force_pts::AbstractMatrix{T},
@@ -244,6 +146,7 @@ function resistance_matrix!(
     A ./= (-T(8) * T(π) * μ)
 end
 
+# if force_pts is a single point, promote to a matrix and then call resistance_matrix!
 function resistance_matrix!(
     A::AbstractMatrix{T},
     force_pts::AbstractVector{T},
@@ -277,9 +180,6 @@ function swimming_matrix!(
         for j in axes(quad_pts, 2)
             Xj = @SVector [quad_pts[1,j], quad_pts[2,j], quad_pts[3,j]] 
             
-            # @inbounds for k in 1:3
-            #     diffvec[k] = force_pts[k,i] - quad_pts[k,j]
-            # end
             n = nearest[j]
             if wall
                 regularised_blakelet!(S, S2, xi, Xj;  eps=eps)
@@ -331,128 +231,71 @@ function swimming_matrix!(
     wall::Bool=false
 ) where {T <: Number}
     @unpack force_pts, quad_pts, nearest = points
-    fill!(A, zero(T))
+    swimming_matrix!(A, x0, force_pts, quad_pts, nearest, eps; μ=μ, wall=wall)
+end
 
-    S = MMatrix{3,3,T}(undef)
-    S2 = MMatrix{3,3,T}(undef)
-    diffvec = MVector{3,T}(undef)
 
-    for i in axes(force_pts, 2)
-        xi = @SVector [force_pts[1,i], force_pts[2,i], force_pts[3,i]]
-        for j in axes(quad_pts, 2)
-            Xj = @SVector [quad_pts[1,j], quad_pts[2,j], quad_pts[3,j]] 
+# function swimming_matrix!(
+#     A::Matrix{T}, 
+#     x0::SVector{3,T}, 
+#     points::NearestDiscretisation,
+#     eps::T; 
+#     μ::T=one(T),
+#     wall::Bool=false
+# ) where {T <: Number}
+#     @unpack force_pts, quad_pts, nearest = points
+#     fill!(A, zero(T))
+
+#     S = MMatrix{3,3,T}(undef)
+#     S2 = MMatrix{3,3,T}(undef)
+#     diffvec = MVector{3,T}(undef)
+
+#     for i in axes(force_pts, 2)
+#         xi = @SVector [force_pts[1,i], force_pts[2,i], force_pts[3,i]]
+#         for j in axes(quad_pts, 2)
+#             Xj = @SVector [quad_pts[1,j], quad_pts[2,j], quad_pts[3,j]] 
             
-            # @inbounds for k in 1:3
-            #     diffvec[k] = force_pts[k,i] - quad_pts[k,j]
-            # end
-            n = nearest[j]
-            if wall
-                regularised_blakelet!(S, S2, xi, Xj;  eps=eps)
-            else
-                R = xi - Xj
-                regularised_stokeslet!(S, R; eps=eps)
-            end
-            @inbounds for p in 1:3, q in 1:3
-                A[3i-3+p, 3n-3+q] -= S[p,q]
-            end
-        end
+#             # @inbounds for k in 1:3
+#             #     diffvec[k] = force_pts[k,i] - quad_pts[k,j]
+#             # end
+#             n = nearest[j]
+#             if wall
+#                 regularised_blakelet!(S, S2, xi, Xj;  eps=eps)
+#             else
+#                 R = xi - Xj
+#                 regularised_stokeslet!(S, R; eps=eps)
+#             end
+#             @inbounds for p in 1:3, q in 1:3
+#                 A[3i-3+p, 3n-3+q] -= S[p,q]
+#             end
+#         end
 
-        @inbounds for p in 1:3
-            A[3i-3+p, end-6+p] = -one(T)  
-            diffvec[p] = force_pts[p,i] - x0[p]
-        end
+#         @inbounds for p in 1:3
+#             A[3i-3+p, end-6+p] = -one(T)  
+#             diffvec[p] = force_pts[p,i] - x0[p]
+#         end
 
-        K = skew_symmetric_static(diffvec)
-        @inbounds for p in 1:3, q in 1:3
-            A[3i-3+p, end-3+q] = K[p,q]
-        end
-    end
+#         K = skew_symmetric_static(diffvec)
+#         @inbounds for p in 1:3, q in 1:3
+#             A[3i-3+p, end-3+q] = K[p,q]
+#         end
+#     end
 
-    nf = length(force_pts)
-    @inbounds A[1:nf, 1:nf] ./= (-T(8) * T(π) * μ)
+#     nf = length(force_pts)
+#     @inbounds A[1:nf, 1:nf] ./= (-T(8) * T(π) * μ)
     
-    for j in axes(quad_pts, 2)
-        n = nearest[j]
+#     for j in axes(quad_pts, 2)
+#         n = nearest[j]
 
-        @inbounds for d in 1:3
-            A[end-6+d, 3n-3+d] += one(T)
-        end
+#         @inbounds for d in 1:3
+#             A[end-6+d, 3n-3+d] += one(T)
+#         end
         
-        rq = SVector{3,T}(quad_pts[1,j], quad_pts[2,j], quad_pts[3,j]) - x0
+#         rq = SVector{3,T}(quad_pts[1,j], quad_pts[2,j], quad_pts[3,j]) - x0
         
-        Kq = skew_symmetric_static(rq)
-        @inbounds for p in 1:3, q in 1:3
-            A[end-3+p, 3n-3+q] += Kq[p,q]
-        end
-    end
-end
-
-function swimming_matrix!(
-    A::Matrix{T},
-    x0::SVector{3,T},
-    points::NystromDiscretisation,
-    eps::T;
-    μ::T = one(T),
-    wall::Bool = false
-) where {T<:Number}
-
-    pts = points.pts              # 3 × N
-    N   = size(pts, 2)
-    fill!(A, zero(T))
-
-    S  = MMatrix{3,3,T}(undef)
-    S2 = MMatrix{3,3,T}(undef)
-    diffvec = MVector{3,T}(undef)
-
-    # Collocation at i, integrate over j (same nodes)
-    @inbounds for i in 1:N
-        xi = @SVector [pts[1,i], pts[2,i], pts[3,i]]
-
-        for j in 1:N
-            Xj = @SVector [pts[1,j], pts[2,j], pts[3,j]]
-
-            if wall
-                regularised_blakelet!(S, S2, xi, Xj; eps=eps)
-            else
-                R = xi - Xj
-                regularised_stokeslet!(S, R; eps=eps)
-            end
-
-            for p in 1:3, q in 1:3
-                A[3i-3+p, 3j-3+q] -= S[p,q]
-            end
-        end
-
-        # Translation columns
-        for p in 1:3
-            A[3i-3+p, end-6+p] = -one(T)
-            diffvec[p] = pts[p,i] - x0[p]
-        end
-
-        # Rotation columns
-        K = skew_symmetric_static(diffvec)
-        for p in 1:3, q in 1:3
-            A[3i-3+p, end-3+q] = K[p,q]
-        end
-    end
-
-    # Scale the kernel block
-    @inbounds A[1:3N, 1:3N] ./= (-T(8) * T(π) * μ)
-
-    # Net force / net torque rows
-    @inbounds for j in 1:N
-        # ∑ f_j
-        for d in 1:3
-            A[end-6+d, 3j-3+d] += one(T)
-        end
-
-        # ∑ r_j × f_j
-        rj = SVector{3,T}(pts[1,j], pts[2,j], pts[3,j]) - x0
-        Kj = skew_symmetric_static(rj)
-        for p in 1:3, q in 1:3
-            A[end-3+p, 3j-3+q] += Kj[p,q]
-        end
-    end
-
-    return A
-end
+#         Kq = skew_symmetric_static(rq)
+#         @inbounds for p in 1:3, q in 1:3
+#             A[end-3+p, 3n-3+q] += Kq[p,q]
+#         end
+#     end
+# end
