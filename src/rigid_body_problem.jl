70bf35cde6f2f00e4502f53c8f6f270f6fcbe570
sara-curtis <sara.drummond-curtis.24@ucl.ac.uk>
2026-08-17 15:40:07 +0100

    updated variable names in rigid body motion

--- src/rigid_body_problem.jl
+++ src/rigid_body_problem.jl
@@ -1,7 +1,7 @@
 # body shape parameter - Bretherton number based on the terms of the body shape tensor B, which is the contraction of the grand mobility matrix and the shear tensor.
 #if the coefficients corresponding to the Bretherton number are not consistent, then the Bretherton number is set to NaN and a warning is printed.
 function body_shape_parameter(ms::MicroSwimmer; eps = 0.1)   
-    B = body_shape_tensor(ms; eps = eps)
+    B = body_mobility_tensor(ms; eps = eps)
     if abs((B[5,1,3])+(B[6,1,2]))< 1e-1
     Breth =  B[6,1,2]- B[5,1,3]
     else
@@ -10,7 +10,9 @@ function body_shape_parameter(ms::MicroSwimmer; eps = 0.1)
     end
     Breth
 end
-function body_shape_tensor(ms::MicroSwimmer; eps = 0.1)   
+#use grand mobility tensor and shear-force/torque tensor to determine mobitly tensor in body fixed frame, 
+#can then be used to calculate rigid body motion byor determine number of independent coefficients in mobility problem
+function body_mobility_tensor(ms::MicroSwimmer; eps = 0.1)   
     ms = body_fixed(ms)
     R = grand_resistance_matrix(ms,eps = eps)
     K = R^-1
@@ -37,17 +39,17 @@ end
 
 #rigid body problem  for  - angular velocity of the body frame in a shear flow with constant shear
 #depends on the  body shape tensor B which is constant in the body frame
-
-function orientation_ode(X,B_shear,strain_shear,Omega_shear,t)
+#tensor contract B_torque with rate of strain of background flow, use body frame basis to simplify calculation 
+function orientation_ode(X,B_torque,strain_shear,Omega_shear,t)
     b1 = X[1:3]
     b2 = X[4:6]
     b3 = cross(b1,b2)
     Q = hcat(b1,b2,b3) 
-    Omega_p =sum((Q*B_shear[1,:,:]*Q').*strain_shear)
-    Omega_q =sum((Q*B_shear[2,:,:]*Q').*strain_shear)
-    Omega_r =sum((Q*B_shear[3,:,:]*Q').*strain_shear)
-    db1 = cross(Omega_shear,b1) - Omega_q*b3 + Omega_r*b2
-    db2 = cross(Omega_shear,b2) + Omega_p*b3 - Omega_r*b1
+    Omega_b1 =sum((Q*B_torque[1,:,:]*Q').*strain_shear)
+    Omega_b2 =sum((Q*B_torque[2,:,:]*Q').*strain_shear)
+    Omega_b3 =sum((Q*B_torque[3,:,:]*Q').*strain_shear)
+    db1 = cross(Omega_shear,b1) - Omega_b2*b3 + Omega_b3*b2
+    db2 = cross(Omega_shear,b2) + Omega_b1*b3 - Omega_b3*b1
     return SVector{6,eltype(X)}(db1..., db2...)
 end
 
@@ -55,7 +57,7 @@ end
 function RigidBodyProblem(ms::MicroSwimmer; 
     Omega_shear = SVector(0.5, 0.0, 0.0),
     strain_shear = SMatrix{3,3}(0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.0, 0.5, 0.0),
-    B_shear = body_shape_tensor(ms; eps = 1e-5)[4:6, :, :],
+    B_torque = body_mobility_tensor(ms; eps = 1e-5)[4:6, :, :],
     t_final=20.0,
     saveat=0.05,
     eps=0.1
@@ -63,7 +65,7 @@ function RigidBodyProblem(ms::MicroSwimmer;
 
     T = Float64
     X0 = SVector{6,T}(ms.frame.orientation[:,1]..., ms.frame.orientation[:,2]...)
-    rhs = (X, p, t) -> orientation_ode(X, B_shear, strain_shear, Omega_shear, t)
+    rhs = (X, p, t) -> orientation_ode(X, B_torque, strain_shear, Omega_shear, t)
     sol = solve(ODEProblem(rhs, X0, (0.0, t_final)), Tsit5(), saveat=saveat)
     u = sol.u
     x = [ms.frame.location for i in eachindex(u)]
